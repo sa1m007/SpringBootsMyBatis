4200dd923c3d2ad3ec5b0b084afd4848e465e666
411411 <411411@PC394389.cts.com>
2018-09-21 16:12:47 +0530

    Adding Swagger

--- leave/src/main/java/com/hrms/leave/LeaveApplication.java
+++ leave/src/main/java/com/hrms/leave/LeaveApplication.java
@@ -2,10 +2,17 @@ package com.hrms.leave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LeaveApplication {
+public class LeaveApplication extends SpringBootServletInitializer{
 
+ @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LeaveApplication.class);
+    }
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveApplication.class, args);
 	}
